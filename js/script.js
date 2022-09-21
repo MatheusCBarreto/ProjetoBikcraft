e28213280486f55f117fb9e42eb9648d956093d3
Matheus <devmattbarreto@protonmail.com>
2022-09-20 22:02:46 -0300

    Adicionando funcionalidades com JavaScript

---
+++ js/script.js
@@ -0,0 +1,45 @@
+// Ativar Links do menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+    const url = location.href;
+    const href = link.href;
+
+    if (url.includes(href)) {
+        link.classList.add("ativo")
+    }
+}
+
+links.forEach(ativarLink);
+
+// Ativar itens do orçamento
+const paramentros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+    const elemento = document.getElementById(parametro);
+    if (elemento) {
+        elemento.checked = true;
+    }
+
+}
+paramentros.forEach(ativarProduto);
+
+// Ativar perguntas
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle("ativa");
+    const ativa = resposta.classList.contains("ativa");
+    pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+    pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
